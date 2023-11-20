a7a4bffab8c7479533187252a65e12563f6759ce
Adina Placintar <adinacraciun19@gmail.com>
2023-11-20 20:59:29 +0200

    add function examples

--- examples.js
+++ examples.js
@@ -1,18 +1,22 @@
-console.info(2 + 3);
-console.warn("Adina Placintar");
+function variablesExamples() {
+  console.info(2 + 3);
+  console.warn("Adina Placintar");
 
-var employed = true;
-var age = 18;
-console.log("Angajat:", employed);
-employed = false;
-console.log("Angajat:", employed);
-employed = "da";
-console.log("Angajat:", employed);
+  var employed = true;
+  var age = 18;
+  console.log("Angajat:", employed);
+  employed = false;
+  console.log("Angajat:", employed);
+  employed = "da";
+  console.log("Angajat:", employed);
 
-var skills = ["html", "css"];
+  var skills = ["html", "css"];
 
-console.debug("tipul variabilei skills:", typeof skills);
+  console.debug("tipul variabilei skills:", typeof skills);
 
-var job = document.getElementById("job-title");
-console.warn("job", job);
-console.info(typeof job);
+  var job = document.getElementById("job-title");
+  console.warn("job", job);
+  console.info(typeof job);
+}
+
+//variablesExamples();
