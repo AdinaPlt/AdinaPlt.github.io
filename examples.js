3e946f86633c0ed9ce661d2217472bce7ed8665e
Adina Placintar <adinacraciun19@gmail.com>
2023-11-20 21:35:20 +0200

    changed job title

--- examples.js
+++ examples.js
@@ -22,5 +22,5 @@ function updateTitle(title) {
   job.innerHTML = "Web Developer Trainee";
 }
 
-//variablesExamples();
-//updateTitle("Web Developer");
+variablesExamples();
+updateTitle("Web Developer Trainee");
