1d96826ed6c21aa2673b925d744017d00265d012
Adina Placintar <adinacraciun19@gmail.com>
2023-11-20 21:37:37 +0200

    changed job-title

--- examples.js
+++ examples.js
@@ -17,10 +17,10 @@ function variablesExamples() {
 
 function updateTitle(title) {
   var job = document.getElementById("job-title");
-  console.warn("job", job, title);
+  console.warn("job", job - title);
   console.info(typeof job);
   job.innerHTML = "Web Developer Trainee";
 }
 
-variablesExamples();
-updateTitle("Web Developer Trainee");
+//variablesExamples();
+//updateTitle("Web Developer");
