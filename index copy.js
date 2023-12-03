46216fa10d3a760f9d3f64d290f7129d5c8e3301
Adina Placintar <adinacraciun19@gmail.com>
2023-12-03 15:15:20 +0200

    hide old page

--- index copy.js
+++ index copy.js
@@ -1,4 +1,7 @@
 function showLanguages() {
+  var oldpage = document.getElementById("projects");
+  oldpage.style.display = "none";
+
   var page = document.getElementById("languages");
   page.style.display = "block";
 }
