565844a2e53759d731af6f3c8bf5232e9b3a62b7
Adina Placintar <adinacraciun19@gmail.com>
2023-12-03 15:22:00 +0200

    updated languages page

--- index copy.js
+++ index copy.js
@@ -1,3 +1,11 @@
+function showSkills() {
+  var oldpage = document.getElementById("projects");
+  oldpage.style.display = "none";
+
+  var page = document.getElementById("skills");
+  page.style.display = "block";
+}
+
 function showLanguages() {
   var oldpage = document.getElementById("projects");
   oldpage.style.display = "none";
