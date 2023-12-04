22f6187cfa3da75116817f693187d88a00468740
Adina Placintar <adinacraciun19@gmail.com>
2023-12-04 18:36:44 +0200

    Added Contact to main menu

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 function showHome() {
+  document.getElementById("skills").style.display = "none";
   document.getElementById("projects").style.display = "none";
   document.getElementById("languages").style.display = "none";
-  document.getElementById("skills").style.display = "none";
+  document.getElementById("contact").style.display = "none";
 
   var page = document.getElementById("home");
   page.style.display = "block";
 }
+
 function showSkills() {
+  document.getElementById("home").style.display = "none";
   document.getElementById("projects").style.display = "none";
   document.getElementById("languages").style.display = "none";
-  document.getElementById("home").style.display = "none";
+  document.getElementById("contact").style.display = "none";
 
   var page = document.getElementById("skills");
   page.style.display = "block";
 }
 
 function showProjects() {
+  document.getElementById("home").style.display = "none";
   document.getElementById("skills").style.display = "none";
   document.getElementById("languages").style.display = "none";
-  document.getElementById("home").style.display = "none";
+  document.getElementById("contact").style.display = "none";
 
   var page = document.getElementById("projects");
   page.style.display = "block";
 }
 
 function showLanguages() {
-  document.getElementById("projects").style.display = "none";
   document.getElementById("home").style.display = "none";
   document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("contact").style.display = "none";
 
   var page = document.getElementById("languages");
+  page.style.display = "block";
+}
+
+function showContact() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+
+  var page = document.getElementById("contact");
   page.style.display = "block";
 }
