b5341ebab59fdf77d275853819e8b6ab42d679b2
Adina Placintar <adinacraciun19@gmail.com>
2024-01-28 12:40:25 +0200

    added more infor to Home and Projects

--- Exercises/Functions.js
+++ Exercises/Functions.js
@@ -60,3 +60,122 @@ var firstName = "Ionel";
 var lastName = "Popescu";
 var fullName = `${firstName} {lastName}`;
 console.info(fullName);
+
+function add(q1, q2, q3, q4) {
+  console.info("sum is: " + (q1 + q2) + q3 + q4++);
+  console.info("last number is:" + q4);
+}
+add(1, 5, 8, 3);
+
+document.body.innerHTML += [
+  '<article id ="q11result">',
+  '<div id = "x">X</div>',
+  '<div name = "y">Y</div>',
+  "</article>";
+var e1 = document.getElementById("x");
+e1.innerHTML = "A";
+var e2 = document.getElementByName("y");
+e1.innerHTML = "B";
+
+console.log(document.getElementById("q11result").innerText);
+
+const numbers [7, 5, 1, 8, 2, 3];
+numbers[2] = 0;
+numbers.sort();
+numbers.push(2);
+numbers.forEach((number) => {
+  if (number % 2 === 0) {
+    console.infor(number);
+  }
+});
+
+document.body.innerHTML += [
+  '<article class="html">',
+  '<div class="clas1">Adrian</div>',
+  '<p id="any">Bichir</p>',
+  '<span class="blue favorite">Cristi</span>',
+  '<ul id="all">',
+  "<li>David</li>",
+  "<li>Florin</li>",
+  "</ul>",
+  "</article>",
+  "<style>",
+  "#cls1, #any, #all li {",
+" color: red;",
+"}",
+"</style>"
+].join("\n");
+
+var model = "320";
+var make = "FORD";
+var car= {
+  make: "BMW",
+  model: "118",
+  info: function () {
+    console.log ("My car is a: " + this.make + " " + model);
+  }
+};
+car.info();
+
+
+function calc(n) {
+  let sum=0;
+  for (let i=1; i<=n; i++){
+    sum+=i;
+  }
+  return sum;
+}
+console.info(calc(4));
+
+function load() {
+  return "ABCDEFGHIJKLMNOP";
+}
+function chained() {
+return load().toLowerCase().substring(2, 4);
+}
+function steps() {
+  const text = load();
+  const lower = text.toLowerCase();
+  const result = lower.substring(2, 4);
+return result;
+}
+const c = chained();
+const s = steps();
+console.info(c);
+console.info(s);
+console.info(c === s);
+
+
+try {
+  var firstName = "Tibi";
+  var person = {
+    firstName: "Andrei",
+    lastName: "Vasilica",
+    call: function () {
+      console.log("My name is: " + firstName + lastName);
+    }
+  };
+  person.call();
+  } catch (e) {
+    console.error(e);
+  }
+
+  function load( {
+    return: ABCDEFGHIJKLMNOP
+  }
+  function chained(){
+    load().toLowerCase().substring(2, 4);
+  }
+  function steps() {
+const text = load();
+const lower = text.toLowerCase();
+const result = lower.substring(2, 4);
+return result;
+  }
+  const c = chained();
+  const s = steps();
+  console.info(c);
+  console.info(s);
+  console.info(c === s);
+
+  
